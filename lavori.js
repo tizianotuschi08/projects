2878346c9acec58110db81cf5d211f2bd303e971
tizianotuschi08 <tiziano.tuschi@gmail.com>
2025-05-27 21:01:33 +0200

    Update lavori.js

--- lavori.js
+++ lavori.js
@@ -15,6 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
       "temperatura_tuschi": "Calcolo della differenza di temperatura",
       "tiro-dadi_tuschi":"Lancio di n dadi con calcolo di media",
       "voto-ristorante_tuschi":"Gestione delle recensioni di ristoranti",
+        "strike&ball_tuschi":"Algoritmo per gioco strike&ball (indovina la combinazione)",
       // Aggiungi qui le descrizioni per ogni esercizio
     };
 
